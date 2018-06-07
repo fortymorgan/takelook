e7a6ca5ca8e5456bb5f7d092d7ae4d93a244f5c6
Forty Morgan <dimazhmudikov@gmail.com>
2018-06-07 19:32:49 +0300

    Add render show page

--- takelook.js
+++ takelook.js
@@ -37,11 +37,13 @@ const renderSearch = async searchValue => {
       img.src = show.image.medium
     }
     img.setAttribute('width','150');
+    img.addEventListener('click', () => renderShow(show._links.self.href));
     listItem.appendChild(img);
-
+    
     const name = document.createElement('h2');
     name.classList.add('show-name');
     name.innerText = show.name;
+    name.addEventListener('click', () => renderShow(show._links.self.href));
     listItem.appendChild(name);
 
     const genres = document.createElement('p');
@@ -53,4 +55,33 @@ const renderSearch = async searchValue => {
     searchResultList.appendChild(listItem);
   });
   contentDiv.appendChild(searchResultList);
+};
+
+const renderShow = async showUrl => {
+  resetResult();
+  const show = document.createElement('div');
+  show.classList.add('show');
+
+  const showData = await fetch(showUrl)
+    .then(blob => blob.json());
+
+  const name = document.createElement('h1');
+  name.classList.add('show-name');
+  name.innerText = showData.name;
+  show.appendChild(name);
+
+  const img = document.createElement('img');
+  if (!showData.image) {
+    img.src = 'no-image.png';
+  } else {
+    img.src = showData.image.medium
+  }
+  show.appendChild(img);
+
+  const summary = document.createElement('div');
+  summary.classList.add('summary');
+  summary.innerHTML = showData.summary;
+  show.appendChild(summary);
+
+  contentDiv.appendChild(show);
 };
