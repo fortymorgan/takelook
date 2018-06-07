5c39107c7b0603a9020c9511b48dec78f02619fa
Forty Morgan <dimazhmudikov@gmail.com>
2018-06-07 21:08:48 +0300

    Add episodes table to show page

--- takelook.js
+++ takelook.js
@@ -19,69 +19,103 @@ const renderSearch = async searchValue => {
   const header = document.createElement('h1');
   header.classList.add('search-head');
   header.innerText = 'Search result';
-  contentDiv.appendChild(header);
+  contentDiv.append(header);
 
   const searchUrl = `http://api.tvmaze.com/search/shows?q=${searchValue.replace(' ', '%20')}`;
   const searchResult = await fetch(searchUrl)
     .then(blob => blob.json());
 
   const searchResultList = document.createElement('ul');
-  searchResult.forEach(result => {
+  searchResult.forEach(({ show }) => {
     const listItem = document.createElement('li');
-    const show = result.show;
+
+    const { image, _links, name, genres } = show;
 
     const img = document.createElement('img');
-    if (!show.image) {
+    if (!image) {
       img.src = 'no-image.png';
     } else {
-      img.src = show.image.medium
+      img.src = image.medium
     }
     img.setAttribute('width','150');
-    img.addEventListener('click', () => renderShow(show._links.self.href));
-    listItem.appendChild(img);
+    img.addEventListener('click', () => renderShow(_links.self));
+    listItem.append(img);
     
-    const name = document.createElement('h2');
-    name.classList.add('show-name');
-    name.innerText = show.name;
-    name.addEventListener('click', () => renderShow(show._links.self.href));
-    listItem.appendChild(name);
+    const showName = document.createElement('h2');
+    showName.classList.add('show-name');
+    showName.innerText = name;
+    showName.addEventListener('click', () => renderShow(_links.self));
+    listItem.append(showName);
 
-    const genres = document.createElement('p');
-    genres.classList.add('genres');
-    const genresList = show.genres.map(genre => `<span class="genre">${genre}</span>`);
-    genres.innerHTML = `Genres ${genresList.join(', ')}`;
-    listItem.appendChild(genres);
+    const showGenres = document.createElement('p');
+    showGenres.classList.add('genres');
+    const genresList = genres.map(genre => `<span class="genre">${genre}</span>`);
+    showGenres.innerHTML = `Genres ${genresList.join(', ')}`;
+    listItem.append(showGenres);
 
-    searchResultList.appendChild(listItem);
+    searchResultList.append(listItem);
   });
-  contentDiv.appendChild(searchResultList);
+  contentDiv.append(searchResultList);
 };
 
-const renderShow = async showUrl => {
+const renderShow = async ({ href }) => {
   resetResult();
   const show = document.createElement('div');
   show.classList.add('show');
 
-  const showData = await fetch(showUrl)
+  const { name, image, summary } = await fetch(href)
     .then(blob => blob.json());
 
-  const name = document.createElement('h1');
-  name.classList.add('show-name');
-  name.innerText = showData.name;
-  show.appendChild(name);
+  const showName = document.createElement('h1');
+  showName.classList.add('show-name');
+  showName.innerText = name;
+  show.append(showName);
 
   const img = document.createElement('img');
-  if (!showData.image) {
+  if (!image) {
     img.src = 'no-image.png';
   } else {
-    img.src = showData.image.medium
+    img.src = image.medium
   }
-  show.appendChild(img);
+  show.append(img);
 
-  const summary = document.createElement('div');
-  summary.classList.add('summary');
-  summary.innerHTML = showData.summary;
-  show.appendChild(summary);
+  const showSummary = document.createElement('div');
+  showSummary.classList.add('summary');
+  showSummary.innerHTML = summary;
+  show.append(showSummary);
 
-  contentDiv.appendChild(show);
+  contentDiv.append(show);
+
+  const episodesData = await fetch(`${href}/episodes`)
+    .then(blob => blob.json());
+
+  const episodes = document.createElement('div');
+  episodes.classList.add('episodes');
+
+  const episodesHeader = document.createElement('h2');
+  episodesHeader.innerText = 'Previous Episodes';
+  episodes.append(episodesHeader);
+
+  const episodesTable = document.createElement('table');
+
+  const thead = document.createElement('thead');
+  const theadRow = document.createElement('tr')
+  theadRow.innerHTML = '<th class="episode">Episode Name</th><th class="airdate">Airdate</th>';
+  thead.append(theadRow);
+  episodesTable.append(thead);
+
+  const tbody = document.createElement('tbody');
+  const tbodyRows = episodesData.map(({ season, number, name, airdate }) => {
+    const nameSpan = `<span class="episode-name">${name}</span>`;
+    const episodeName = `${season}x${number < 10 ? '0' + number: number}: ${nameSpan}`;
+    const episodeAirdate = moment(airdate).format('MMM DD, YYYY');
+    return `<td class="episode">${episodeName}</td><td class="airdate">${episodeAirdate}</td>`;
+  }).forEach(row => {
+    const tableRow = document.createElement('tr');
+    tableRow.innerHTML = row;
+    tbody.prepend(tableRow);
+  });
+  episodesTable.append(tbody);
+
+  contentDiv.append(episodesTable);
 };
